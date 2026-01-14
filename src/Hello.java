0e42abd76f3d689d4f5d7067d4270f6b27691d9f
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-01-14 23:09:56 +0800

    Create Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("🎯 Hello from single-file Java!");
+        System.out.println("I love Gradle build system.");
+        System.out.println("Current time: " + java.time.LocalDateTime.now());
+    }
+}
