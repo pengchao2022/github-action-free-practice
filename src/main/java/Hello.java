038f68f080186dcf9bd642cf436e65d031bcbd5d
pengchao <pengchao.ma2@outlook.com>
2026-01-15 12:20:36 +0800

    Modified the code

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,9 @@ public class Hello {
         System.out.println("🎯 Hello from single-file Java!");
         System.out.println("I love Gradle build system.");
         System.out.println("My name is Pengchao Ma");
+        System.out.println("I am from Shanghai China");
+        System.out.println("I am a software engineer");
+        system.out.println("This repo is for demoing github action github ci practice");
         System.out.println("Current time: " + java.time.LocalDateTime.now());
 
     }
